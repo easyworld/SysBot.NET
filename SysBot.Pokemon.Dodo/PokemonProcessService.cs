666a6e7fe14f7c59dbc00952207b5391a7830c04
Sakura <106369723+xiaolong11123@users.noreply.github.com>
2023-04-17 05:58:42 +0800

    Add files via upload

--- SysBot.Pokemon.Dodo/PokemonProcessService.cs
+++ SysBot.Pokemon.Dodo/PokemonProcessService.cs
@@ -15,7 +15,7 @@ namespace SysBot.Pokemon.Dodo
     {
         private readonly OpenApiService _openApiService;
         private static readonly string LogIdentity = "DodoBot";
-        private static readonly string Welcome = "at我并尝试对我说：\n皮卡丘\ntrade ps代码\n或者直接拖一个文件进来";
+        private static readonly string Welcome = "at我并尝试对我说：\n皮卡丘\nps代码(首字母大写)\n或者直接拖一个文件进来";
         private readonly string _channelId;
         private DodoSettings _dodoSettings;
         private string _botDodoSourceId = default!;
@@ -97,7 +97,7 @@ namespace SysBot.Pokemon.Dodo
             if (!content.Contains($"<@!{_botDodoSourceId}>")) return;
 
             content = content.Substring(content.IndexOf('>') + 1);
-            if (typeof(TP) == typeof(PK9) && content.Contains("\n\n") && ShowdownTranslator<TP>.IsPS(content))// 仅SV支持批量，其他偷懒还没写
+            if ((typeof(TP) == typeof(PK9) || typeof(TP) == typeof(PK8) || typeof(TP) == typeof(PA8)) && content.Contains("\n\n") && ShowdownTranslator<TP>.IsPS(content))
             {
                 ProcessWithdraw(eventBody.MessageId);
                 new DodoTrade<TP>(ulong.Parse(eventBody.DodoSourceId), eventBody.Personal.NickName, eventBody.ChannelId, eventBody.IslandSourceId).StartTradeMultiPs(content.Trim());
@@ -115,7 +115,7 @@ namespace SysBot.Pokemon.Dodo
                 new DodoTrade<TP>(ulong.Parse(eventBody.DodoSourceId), eventBody.Personal.NickName, eventBody.ChannelId, eventBody.IslandSourceId).StartDump();
                 return;
             }
-            else if (typeof(TP) == typeof(PK9) && content.Trim().Contains('+'))// 仅SV支持批量，其他偷懒还没写
+            else if ((typeof(TP) == typeof(PK9) || typeof(TP) == typeof(PK8) || typeof(TP) == typeof(PA8)) && content.Trim().Contains('+'))
             {
                 ProcessWithdraw(eventBody.MessageId);
                 new DodoTrade<TP>(ulong.Parse(eventBody.DodoSourceId), eventBody.Personal.NickName, eventBody.ChannelId, eventBody.IslandSourceId).StartTradeMultiChinesePs(content.Trim());
